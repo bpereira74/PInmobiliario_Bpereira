b76769c0c9006b5cd847062941ab982ef51d3774
bpereira74 <bpereira74@hotmail.com>
2023-08-22 09:20:01 -0400

    feat: Scaffold contact

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :contacts
                   get '/home',    to: 'pages#home'
   		      get '/privacy', to: 'pages#privacy'
 		      get '/legal',   to: 'pages#terms'
