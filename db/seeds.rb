6fb5aeb09d34c70723aa379895e8267c1b7a769b
bpereira74 <bpereira74@hotmail.com>
2023-08-22 10:03:13 -0400

    feat: seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
 require 'csv'
 puts 'Importing categories...'
 CSV.foreach(Rails.root.join('db/seeds/csv/operations.csv'), headers: true) do |row|
-  Category.create! do |category|
-    category.id = row[0]
-    category.name = row[1]
-    category.available = row[2]
+  Operation.create! do |operation|
+    operation.id = row[0]
+    operation.name = row[1]
+    operation.available = row[2]
   end
 end
