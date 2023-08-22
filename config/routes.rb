85fb85320014fec73649549ae9d2303fe9f023af
bpereira74 <bpereira74@hotmail.com>
2023-08-22 10:23:19 -0400

    feat: gema devise

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
                   get '/home',    to: 'pages#home'
   		      get '/privacy', to: 'pages#privacy'
 		      get '/legal',   to: 'pages#terms'
-		      root 'pages#home'
+                  root to: "home#index"
+		      # root 'pages#home'
 resources :contacts, only: %i[new create]
 end
