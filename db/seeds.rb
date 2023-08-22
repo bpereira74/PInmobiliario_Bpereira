7b29704e1de25fdfad7969f2d7b544879d7b327a
bpereira74 <bpereira74@hotmail.com>
2023-08-22 10:00:19 -0400

    feat: commit seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'csv'
+puts 'Importing categories...'
+CSV.foreach(Rails.root.join('db/seeds/csv/operations.csv'), headers: true) do |row|
+  Category.create! do |category|
+    category.id = row[0]
+    category.name = row[1]
+    category.available = row[2]
+  end
+end
